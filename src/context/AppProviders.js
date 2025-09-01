4444cb4f9c098bbe679e9443292d0ce2831cf133
JEER11 <127362314+JEER11@users.noreply.github.com>
2025-09-01 01:38:54 -0400

    fix: mount AppointmentProvider globally so dashboard/useAppointments works; prevent context undefined on /dashboard

--- src/context/AppProviders.js
+++ src/context/AppProviders.js
@@ -2,13 +2,16 @@
 import React from "react";
 import { BillingProvider } from "context/BillingContext";
 import { NotificationProvider } from "context/NotificationContext";
+import { AppointmentProvider } from "context/AppointmentContext";
 
 export default function AppProviders({ children }) {
   return (
-    <BillingProvider>
-      <NotificationProvider>
-        {children}
-      </NotificationProvider>
-    </BillingProvider>
+    <AppointmentProvider>
+      <BillingProvider>
+        <NotificationProvider>
+          {children}
+        </NotificationProvider>
+      </BillingProvider>
+    </AppointmentProvider>
   );
 }
