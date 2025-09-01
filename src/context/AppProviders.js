979274e2ee28cc8b1f110301f1d7c1b6a76bcaa6
JEER11 <127362314+JEER11@users.noreply.github.com>
2025-09-01 01:47:33 -0400

    fix: wrap app with TodoProvider as well; resolves undefined useTodos on dashboard

--- src/context/AppProviders.js
+++ src/context/AppProviders.js
@@ -3,13 +3,16 @@ import React from "react";
 import { BillingProvider } from "context/BillingContext";
 import { NotificationProvider } from "context/NotificationContext";
 import { AppointmentProvider } from "context/AppointmentContext";
+import { TodoProvider } from "context/TodoContext";
 
 export default function AppProviders({ children }) {
   return (
     <AppointmentProvider>
       <BillingProvider>
         <NotificationProvider>
-          {children}
+          <TodoProvider>
+            {children}
+          </TodoProvider>
         </NotificationProvider>
       </BillingProvider>
     </AppointmentProvider>
